317e049f1c64f4e5662e1cc7000600a25b6aa542
therealharshit <therealharshit014@gmail.com>
2025-01-06 00:31:17 +0530

    define methods in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
-
+import { ToDoContextProvider } from "./context/ToDoContext";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prevTodos) => [{ id: Date.now(), ...todo }, ...prevTodos]); // we can take access of previous values using callback
+  };
+
+  const deleteToDo = (id) => {
+    setTodos((prevTodos) => {
+      prevTodos.filter((todo) => {
+        return todo.id !== id;
+      });
+    });
+  };
+
+  const updateToDo = (id, todo) => {
+    setTodos((prevTodos)=>{
+      prevTodos.map((prevTodo)=>{
+        return (prevTodo.id === id) ? todo : prevTodo;
+      })
+    })
+  };
+
+  const toggleComplete =(id) => {
+    setTodos((prevTodos)=>{
+      prevTodos.map((prevTodo)=>{
+        return (prevTodo.id === id) ? {...prevTodo, comleted: !prevTodo.cpmpleted} : prevTodo;
+      })
+    })
+  };
 
   return (
-    <>
-    <h1 className="text-3xl font-bold underline text-center">
-      Hello world!
-    </h1>
-    </>
-  )
+    <ToDoContextProvider
+      value={{ todos, addTodo, deleteToDo, updateToDo, toggleComplete }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </ToDoContextProvider>
+  );
 }
 
-export default App
+export default App;
